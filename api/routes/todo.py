e6f01f44c3b58cd5671062d24fc434943fa43efb
Birva Babaria <birvababaria@gmail.com>
2025-04-15 16:31:39 -0700

    Final Push

--- api/routes/todo.py
+++ api/routes/todo.py
@@ -1,19 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from api.schemas.todo import GetTodo, PostTodo, PutTodo
+from api.models.todo import Todo
 
 todo_router = APIRouter(prefix="/api", tags=["Todo"])
 
 @todo_router.get("/")
-def all_todos():
-    return "Not Implemented"
+async def all_todos():
+    data = Todo.all()
+    return await GetTodo.from_queryset(data)
 
 @todo_router.post("/")
-def post_todo():
-    return "Not Imp"
+async def post_todo(body: PostTodo):
+    data = body.dict(exclude_unset=True)
+    tuple = await Todo.create(**data)
+    return await GetTodo.from_tortoise_orm(tuple)
 
 @todo_router.put("/{key}")
-def update_todo(key):
-    return key
+async def update_todo(key: int, body: PutTodo):
+    data = body.dict(exclude_unset=True)
+    hasTuple = await Todo.filter(id=key).exists()
+    if not hasTuple:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found:(")
+    await Todo.filter(id=key).update(**data)
+    return await GetTodo.from_queryset_single(Todo.get(id=key))
 
 @todo_router.delete("/{key}")
-def delete_todo(key):
-    return key
+async def delete_todo(key: int):
+    hasTuple = await Todo.filter(id=key).exists()
+    if not hasTuple:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found:(")
+    await Todo.filter(id=key).delete()
+    return "Todo Deleted!"
